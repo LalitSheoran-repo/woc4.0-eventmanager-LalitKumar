e0497fa32b20477c8327cbf752f19eae583e786c
Lalit <76957686+LalitSheoran-repo@users.noreply.github.com>
2022-01-16 23:43:50 +0530

    Add files via upload

---
+++ Pyscripts_midEvalulation/CompressImages.py
@@ -0,0 +1,28 @@
+# Aim of this script is to compress the image without losing image quality.
+from PIL import Image
+import os
+
+
+def compress_image():
+
+    try:
+        image_name = str(input("Enter the name of the image you want to compress: "))
+        default_image = Image.open(f'{image_name}')
+    except FileNotFoundError:
+        default_image = Image.open('original_image.jpg')
+
+    # Get image 'width' and 'height'
+    w, h = default_image.size
+
+    # Separate the file name from the extension
+    default_image_name = os.path.splitext(os.path.basename(default_image.filename))
+
+    # compress image
+    default_image = default_image.resize((w, h), Image.ANTIALIAS)
+
+    # return compressed image
+    return default_image.save('{}_compressed{}'.format(default_image_name[0], default_image_name[1]))
+
+
+# Run
+compress_image()
